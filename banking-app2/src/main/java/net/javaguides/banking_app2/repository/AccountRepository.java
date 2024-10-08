a19638eaa418571ddc7789b9022c41c296921c52
Saadhvi <151912003+saadhvicancode@users.noreply.github.com>
2024-10-08 21:07:03 +0530

    Delete banking-app2/src/main directory

--- banking-app2/src/main/java/net/javaguides/banking_app2/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.banking_app2.repository;
-
-import net.javaguides.banking_app2.entity.Account;
-import net.javaguides.banking_app2.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    void delete(Account account);
-}

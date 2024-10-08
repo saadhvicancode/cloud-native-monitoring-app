775ec6c2e9ae1065f502e577702fd7b4a091f46e
Saadhvi <151912003+saadhvicancode@users.noreply.github.com>
2024-10-08 21:07:14 +0530

    Delete movieAPI directory

--- movieAPI/src/main/java/com/movieflix/dto/MailBody.java
+++
@@ -1,7 +0,0 @@
-package com.movieflix.dto;
-
-import lombok.Builder;
-
-@Builder
-public record MailBody(String to, String subject, String text) {
-}
--- movieAPI/src/main/java/com/movieflix/repositories/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.movieflix.repositories;
-
-import com.movieflix.entities.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
